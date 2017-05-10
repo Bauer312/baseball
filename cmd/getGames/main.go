326ef9f01f7a263904da0ec0cd21349426efbe23
Brian Bauer <bbauer@outlook.com>
2017-05-09 23:58:14 -0500

    Get base URL for all games

--- cmd/getGames/main.go
+++ cmd/getGames/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"strings"
+
+	"golang.org/x/net/html"
 
 	"github.com/Bauer312/baseball/pkg/dateslice"
+	"github.com/Bauer312/baseball/pkg/util"
 )
 
 func main() {
@@ -18,7 +23,44 @@ func main() {
 
 	if ds != nil {
 		for _, d := range ds {
-			fmt.Println(d)
+			util.SetRoot("http://gd2.mlb.com/components/game/mlb", "/usr/local/share/baseball")
+			dateURL, err := util.DateToURL(d)
+			if err != nil {
+				fmt.Println(err)
+			}
+			dateFS, err := util.URLToFSPath(dateURL)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fileReader, err := os.Open(dateFS)
+			if err != nil {
+				fmt.Println(err)
+			}
+			defer fileReader.Close()
+			htmlTokenizer := html.NewTokenizer(fileReader)
+			for {
+				tt := htmlTokenizer.Next()
+				if tt == html.ErrorToken {
+					break
+				}
+				if tt == html.StartTagToken {
+					t := htmlTokenizer.Token()
+
+					isAnchor := t.Data == "a"
+					if isAnchor {
+						for _, a := range t.Attr {
+							if a.Key == "href" {
+								if strings.HasPrefix(a.Val, "gid_") {
+									fmt.Printf("%s%s\n", dateURL, a.Val)
+									//fmt.Println("Found href:", a.Val)
+								}
+
+								break
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 }
